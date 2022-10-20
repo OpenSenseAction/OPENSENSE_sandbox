8a5de128bf43f4bdd4c0404d78c8689d94ccd832
Christian Chwala <christian.chwala@kit.edu>
2022-10-20 11:49:23 +0200

    Do init of repo content from index notebook
    I added a small init script and a new cell to the index notebook to run
    it.
    
    Note that the script cannot just be run via `!bash init.sh` because this
    will run in the wrong conda env.

---
+++ notebooks/init.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+source activate opensense_sandbox
+
+echo 'Running with the following python binary'
+which python
+
+cd ..
+
+echo ''
+echo 'get content of submodules...'
+git submodule update --init
+
+echo ''
+echo 'add python submodules to conda env...'
+conda develop pycomlink
+conda develop PyNNcml
+# not sure if we need this one since it currently is not 
+# used in the example notebook of Abbas which defines all
+# required functions inside the notebook
+conda develop pws-pyqc
+
+echo ''
+echo 'Install RAINLINK...'
+cd RAINLINK
+R CMD INSTALL RAINLINK*.tar.gz
+
+echo ''
+echo 'create dir in RAINLINK for figures...'
+mkdir Figures
+
+echo ''
+echo '...done'
